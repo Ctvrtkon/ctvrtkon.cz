246e5206c01dd057ce35297da19b21e7b79fa899
Michal Haták <me@twista.cz>
2018-10-14 17:52:59 +0200

    PWA + favicon - closes #5

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,6 +13,12 @@ const TemplateWrapper = ({ children }) => (
       <html lang="cs"/>
       <meta charSet="utf-8"/>
       <meta name="description" content="Čtvrteční setkání webové komunity v Českých Budějovicích"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png"/>
+      <link rel="manifest" href="/img/favicons/site.webmanifest"/>
+      <link rel="mask-icon" href="/img/favicons/safari-pinned-tab.svg" color="#5bbad5"/>
+      <meta name="msapplication-TileColor" content="#da532c"/>
+      <meta name="theme-color" content="#ffffff"/>
     </Helmet>
     <Navbar/>
     <div>
