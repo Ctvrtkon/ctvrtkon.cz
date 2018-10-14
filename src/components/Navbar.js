f51cac905b4d0838063505f938933182014acd13
Michal Haták <me@twista.cz>
2018-10-14 05:07:10 +0200

    Add active state to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo-alternative.svg'
+import React from "react";
+import { Link } from "gatsby";
+import github from "../img/github-icon.svg";
+import logo from "../img/logo-alternative.svg";
 
 const Navbar = () => (
   <nav className="navbar is-primary is-uppercase has-text-weight-bold">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <img src={logo} alt="Čtvrtkon" />
+          <img src={logo} alt="Čtvrtkon"/>
         </Link>
       </div>
       <div className="navbar-start">
-        <Link className="navbar-item" to="/posts">
+        <Link className="navbar-item" activeClassName="is-active" to="/posts">
           Pozvánky
         </Link>
-        <Link className="navbar-item" to="/about">
+        <Link className="navbar-item" activeClassName="is-active" to="/about">
           O Projektu
         </Link>
-        <Link className="navbar-item" to="/contact">
+        <Link className="navbar-item" activeClassName="is-active" to="/contact">
           Kontakt
         </Link>
       </div>
@@ -30,13 +30,13 @@ const Navbar = () => (
           rel="noopener noreferrer"
         >
           <span className="icon">
-            <img src={github} alt="Github" />
+            <img src={github} alt="Github"/>
           </span>
         </a>
       </div>
     </div>
   </nav>
-)
+);
 
-export default Navbar
+export default Navbar;
 
