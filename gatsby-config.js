42f524ad92f377c70d67b86836780b69615af232
Michal Haták <me@twista.cz>
2018-10-15 21:51:43 +0200

    Lib upgrade + offline support (#11) + assets cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,68 @@
 module.exports = {
   siteMetadata: {
-    title: 'ctvrtkon.cz',
+    title: "ctvrtkon.cz"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+        name: "pages"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
-      },
+        name: "images"
+      }
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
-        plugins: [],
-      },
+        plugins: []
+      }
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+        name: `Čtvrtkon.cz`,
+        short_name: `Čtvrtkon`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#007CA3`,
+        display: `minimal-ui`,
+        icon: `static/img/icon-512x512.png`,
+        icons: [
+          {
+            "src": "static/img/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "static/img/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "static/img/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
+      }
     },
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
-}
+    "gatsby-plugin-offline",
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
+    },
+    "gatsby-plugin-netlify" // make sure to keep it last in the array
+  ]
+};
