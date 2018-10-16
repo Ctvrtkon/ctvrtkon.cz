ca6a9d1f0e25e6398e7885531a5cec783eb22aef
Michal Haták <me@twista.cz>
2018-10-16 08:52:23 +0200

    exclude contact form from cache

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -22,7 +22,7 @@ export class ContactForm extends Component {
   };
 
   handleSubmit = e => {
-    fetch("/", {
+    fetch("/?no-cache=1", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...this.state })
