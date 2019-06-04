818abd19204acc7bd97d59811582e5a9a961ab07
Michal Haták <me@twista.cz>
2019-06-04 17:31:54 +0200

    register competition admin page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,9 @@ import CMS from "netlify-cms";
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import ContactPagePreview from "./preview-templates/ContactPagePreview";
+import CompetitionPagePreview from "./preview-templates/CompetitionPagePreview";
 
 CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("contact", ContactPagePreview);
 CMS.registerPreviewTemplate("post", BlogPostPreview);
+CMS.registerPreviewTemplate("competition", CompetitionPagePreview);
