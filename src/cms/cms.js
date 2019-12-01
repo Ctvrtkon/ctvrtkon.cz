4a33497ac0c19987847c4951778f2e4eea5cd1f4
Michal Haták <me@twista.cz>
2019-12-01 15:21:09 +0100

    use netlify-cms instead of netlify-cms-app

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,4 @@
-import CMS from 'netlify-cms-app'
+import CMS from "netlify-cms";
 
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
